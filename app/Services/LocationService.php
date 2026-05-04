4a770efb3992633160f4c9cac825d9a3520167a7
Umairah Sabri <n.umairahsabri@gmail.com>
2026-05-04 08:30:22 +0800

    add logic function in LocationService

--- app/Services/LocationService.php
+++ app/Services/LocationService.php
@@ -2,10 +2,22 @@
 
 namespace App\Services;
 
+/**
+ * LocationService - Menguruskan laluan storan dan aset sistem
+ */
 class LocationService
 {
     /**
-     * Laluan aset awam (public/assets/)
+     * Dapatkan laluan fizikal folder storan (writable)
+     */
+    public function getStorage()
+    {
+        // Guna WRITEPATH untuk simpan fail sulit seperti PDF surat tawaran
+        return WRITEPATH . 'uploads/';
+    }
+
+    /**
+     * Dapatkan URL untuk aset awam (CSS/JS/Imej)
      */
     public function getAssets()
     {
@@ -13,10 +25,11 @@ class LocationService
     }
 
     /**
-     * Laluan template Metronic Demo2
+     * Dapatkan URL khusus untuk template Metronic v8.3.3 Demo 2
+     * Penting untuk panggil plugins.bundle.css/js
      */
     public function getMetronic()
     {
-        return base_url('assets/templates/metronic/html/d2/');
+        return base_url('assets/templates/metronic/html/d2/assets/');
     }
 }
